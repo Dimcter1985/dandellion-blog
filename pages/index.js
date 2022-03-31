9e84bd93364772c513e44e7d2e2657ddc8792953
Рыбников Д <rb@ooo31.com>
2022-03-31 16:58:27 +0700

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,40 @@
-const Home = () => {
+
+import { client } from '../utils/prismicPosts';
+import Post from '../components/Post';
+
+const Home = ({posts}) => {
   return (
-    <h1>Dandelion Blog</h1>
+    <>
+      <h1>Dandelion Blog</h1>
+      <div className="posts">
+        {posts !== undefined &&
+          posts.map((p) => {
+            let title = p.title[0].text
+            let key = `${p.date}+${title}`
+            return <Post key={key} date={p.date} image={p.image} title={title} />
+          })}
+      </div>
+    </>
+
   )
 }
 export default Home;
+
+
+
+// at the bottom of your component file
+export async function getStaticProps() {
+  // query() is empty on purpose!
+  // https://prismic.io/docs/rest-api/query-the-api/query-all-documents
+  const res = await client.query('')
+
+  const posts = res.results.map((p) => {
+    return p.data
+  })
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
